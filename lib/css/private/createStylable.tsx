a8ea86944eb757ff299670ea122ab50c4423c64c
grzechz <grzegorz.adam.zadora@gmail.com>
2022-06-02 09:35:21 +0200

    Set up forwardRef for stylables

--- lib/css/private/createStylable.tsx
+++ lib/css/private/createStylable.tsx
@@ -2,6 +2,7 @@ import {
   createElement,
   DetailedHTMLFactory,
   DetailedHTMLProps,
+  forwardRef,
   ReactHTML,
   ReactNode,
 } from "react";
@@ -12,27 +13,33 @@ export function createStylable<
   T extends keyof ReactHTML,
   E extends ReactHTML[T]
 >(htmlElement: T) {
-  return ({
-    css,
-    cssIdentifier,
-    className: externalClassName,
-    ...props
-  }: PropsFromHtml<T, E> & {
-    css: CssObject;
-    cssIdentifier?: string;
-    className?: string;
-    children?: ReactNode;
-  }) => (
-    <StyleBuilder css={css} identifier={cssIdentifier}>
-      {(className) =>
-        createElement(htmlElement, {
-          ...props,
-          className: externalClassName
-            ? `${externalClassName} ${className}`
-            : className,
-        })
-      }
-    </StyleBuilder>
+  return forwardRef(
+    (
+      {
+        css,
+        cssIdentifier,
+        className: externalClassName,
+        ...props
+      }: PropsFromHtml<T, E> & {
+        css: CssObject;
+        cssIdentifier?: string;
+        className?: string;
+        children?: ReactNode;
+      },
+      forwardedRef
+    ) => (
+      <StyleBuilder css={css} identifier={cssIdentifier}>
+        {(className) =>
+          createElement(htmlElement, {
+            ...props,
+            ref: forwardedRef,
+            className: externalClassName
+              ? `${externalClassName} ${className}`
+              : className,
+          })
+        }
+      </StyleBuilder>
+    )
   );
 }
 
