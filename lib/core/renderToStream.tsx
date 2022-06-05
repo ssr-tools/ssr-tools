74dcc5b0ea9b64fb896525cf78296ba3e8fc6620
grzechz <grzegorz.adam.zadora@gmail.com>
2022-06-05 12:26:43 +0200

    [@ssr-tools/core] Fix types of renderToStream

--- lib/core/renderToStream.tsx
+++ lib/core/renderToStream.tsx
@@ -30,7 +30,8 @@ export const renderToStream = ({
     bootstrapScripts: [path.join(staticAssetsPrefix, manifestClient["main"])],
     onShellReady: () => {
       onShellReady?.();
-      return pipe(stream);
+      pipe(stream);
+      return;
     },
     ...pipeableStreamOptions,
   });
