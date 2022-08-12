405075cc184ca565974b44b9c29d19dacb9e2105
grzechz <grzechz@protonmail.com>
2022-08-12 13:19:00 +0200

    refactor(core): remove hydrateStream method

--- examples/with-fastify/client.tsx
+++ examples/with-fastify/client.tsx
@@ -1,14 +1,12 @@
-import { hydrateStream } from "@ssr-tools/core/hydrateStream";
+import { hydrateRoot } from "react-dom/client";
 import { App } from "./components/App";
 import { Providers } from "./components/Providers.client";
 
 const root = document.getElementById("root");
 
-if (!root) {
-  throw new Error("Cannot find root element");
-}
+if (!root) throw new Error("Cannot find root element");
 
-hydrateStream(
+hydrateRoot(
   root,
   <Providers>
     <App />
--- lib/core/hydrateStream.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-import ReactDom from "react-dom/client";
-
-/**
- * Use it with combination with `renderToStream` on the server
- */
-export const hydrateStream = (root: HTMLElement, element: ReactElement) =>
-  ReactDom.hydrateRoot(root, element);
